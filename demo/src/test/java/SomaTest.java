445a065349d152853f01441e2ba9b9369e7f420f
Daniel Bezerra <daniel.bezerra92@gmail.com>
2026-02-28 15:04:11 +0000

    novo

---
+++ demo/src/test/java/SomaTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.Soma;
+
+public class SomaTest {
+
+    @Test
+    public void operacaoTest(){
+        Soma s = new Soma(3, 5);
+        int resultado = s.operacao();
+        assertEquals(8, resultado);
+    }
+
+    @Test
+    public void helloTest(){
+        Soma s = new Soma(2,3);
+        assertEquals("Hello world!", s.hello());
+    }
+    
+}
