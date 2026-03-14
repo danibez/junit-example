775d06a8040fbd5381528e83aee7ba848d325e3e
Daniel Bezerra <daniel.bezerra92@gmail.com>
2026-03-14 14:47:41 +0000

    calculadora

--- demo/src/main/java/com/example/Calculadora.java
+++ demo/src/main/java/com/example/Calculadora.java
@@ -2,26 +2,22 @@ package com.example;
 
 public class Calculadora {
 
-    // Soma dois números
-    public int somar(int a, int b) {
-        return a + b;
+    public int soma(int a, int b){
+        return a+b;
     }
 
-    // Subtrai dois números
-    public int subtrair(int a, int b) {
-        return a - b;
+    public int sub(int a, int b){
+        return a-b;
     }
 
-    // Divide dois números
-    public double dividir(int a, int b) {
-        if (b == 0) {
+    public int mult(int a, int b){
+        return a*b;
+    }
+
+    public double div(int a, int b){
+        if (b==0) {
             throw new IllegalArgumentException("Divisor não pode ser zero");
         }
-        return (double) a / b;
-    }
-
-    // Multiplica dois números
-    public int multiplicar(int a, int b) {
-        return a * b;
+        return a/b;   
     }
 }
